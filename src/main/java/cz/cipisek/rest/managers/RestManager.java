8422db6f991d07e2f34bde4927fb12f24935fdd1
Cipiman1973 <62877816+Cipiman1973@users.noreply.github.com>
2021-02-11 19:25:06 +0100

    rest

---
+++ src/main/java/cz/cipisek/rest/managers/RestManager.java
@@ -0,0 +1,82 @@
+package cz.cipisek.rest.managers;
+
+
+import javax.enterprise.context.ApplicationScoped;
+import javax.ws.rs.core.Response;
+import java.io.Serializable;
+import java.util.HashMap;
+
+
+@ApplicationScoped
+public class RestManager implements Serializable {
+
+
+    public static Object getZviratko;
+    static HashMap<Integer, HashMap<String, String>> zviratko = new HashMap<>();
+    static Integer number = 0;
+    static String vek;
+    static String nazev;
+
+
+    public static void getInZviratko() {
+        if (number != null && zviratko.get(number) != null) {
+            Response.ok(zviratko.get(number)).build();
+        } else {
+            Response.ok("zviratko neexistuje").build();
+        }
+    }
+
+    public static void deleteZviratko() {
+        if (number != null) {
+            zviratko.remove(number);
+        }
+    }
+
+    public static void noveZviratko() {
+
+        if (nazev != null && vek != null) {
+            HashMap<String, String> noveZviratko = new HashMap<>();
+            noveZviratko.put("nazev", nazev);
+            noveZviratko.put("vek", vek);
+            zviratko.put(number, noveZviratko);
+            number++;
+            Response.ok(zviratko).build();
+        } else {
+            Response.ok("Vlozte jmeno a vek zviratka").build();
+        }
+    }
+
+    public static void zmenitZviratko(){
+        if(zviratko.get(number) != null && number != null && nazev != null && vek != null){
+            HashMap<String,String> noveZviratko = new HashMap<>();
+            noveZviratko.put("nazev", nazev);
+            noveZviratko.put("vek", vek);
+            zviratko.put(number, noveZviratko);
+        }
+        Response.ok(zviratko).build();
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
